1dcc75631407a6e7fb7c3a2a6644b252b8446cd2
Sven Anders Robbestad <robbestad@gmail.com>
2015-06-18 22:42:06 +0200

    Fetching UV data from API

--- index.ios.js
+++ index.ios.js
@@ -1,7 +1,6 @@
 'use strict';
-
 var React = require('react-native');
-
+var Request = require('superagent');
 var {
   AppRegistry,
   StyleSheet,
@@ -9,35 +8,52 @@ var {
   NavigatorIOS,
   View,
   } = React;
-
 var styles = require("./styles");
 var uvapp = React.createClass({
   getInitialState(){
     return {
       longitude: 0,
-      latitude: 0
+      latitude: 0,
+      apiData: {}
     }
   },
-
+  fetchAPIData(){
+    var that = this;
+    var long = this.state.longitude.toString().replace(".",",");
+    var lat = this.state.latitude.toString().replace(".",",");
+    Request
+      .get('http://uvapi.herokuapp.com/forecast/longitude='+long+'&latitude='+lat)
+      .set('Accept', 'application/json')
+      .end(function (err, res) {
+        that.setState({
+          apiData: res
+        })
+      });
+  },
+  componentWillUpdate(nextProps,nextState){
+    if(nextState.longitude!==0){
+      this.fetchAPIData();
+    }
+  },
   fetchCoordinates(){
     var that = this;
 
-    function success(pos){
+    function success(pos) {
       var coords = pos.coords;
       that.setState({
         latitude: coords.latitude,
         longitude: coords.longitude
       });
     }
+
     function error(err) {
       console.warn('ERROR(' + err.code + '): ' + err.message);
     }
+
     navigator.geolocation.getCurrentPosition(success, error, null);
   },
-
   componentDidMount(){
     this.fetchCoordinates();
-
   },
   render: function () {
     return (
@@ -56,10 +72,12 @@ var uvapp = React.createClass({
           <Text style={styles.instructions}>
             Lat: {this.state.latitude}
           </Text>
+          <Text style={styles.instructions}>
+            {this.state.apiData}
+          </Text>
         </View>
       </View>
     );
   }
 });
-
 AppRegistry.registerComponent('uvapp', () => uvapp);
